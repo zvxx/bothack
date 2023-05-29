8fdcb138a0091a38319ba2ebab7f972db6324390
Levi <118043279+zvxx@users.noreply.github.com>
2023-05-29 10:35:44 +0300

    Update boty.py

--- boty.py
+++ boty.py
@@ -2,7 +2,7 @@ import telebot
 
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = ""
+TOKEN = "6072492275:AAE0UaZyuxbKsunZ2i0DxQVGheR0NDXBw-c"
 
 bot = telebot.TeleBot(TOKEN)
 
